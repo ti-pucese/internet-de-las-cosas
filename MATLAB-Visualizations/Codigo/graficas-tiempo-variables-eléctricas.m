d78fb1389c2ab498a850b986d4a7cc32609d9d0f
Ami2610 <amigdiofigueroa@hotmail.com>
2026-07-01 22:39:22 -0500

    Agrego carpeta de visualizaciones MATLAB

---
+++ MATLAB-Visualizations/Codigo/graficas-tiempo-variables-eléctricas.m
@@ -0,0 +1,66 @@
+% Gráficas de tiempo de variables eléctricas
+
+% ID del canal ThingSpeak
+channelID = 3410710;
+
+% Clave de lectura del canal
+readAPIKey = 'Z4G6S9WQJAH23QOS';
+
+% Lectura de los últimos 100 datos enviados por el ESP32
+[data, timeStamps] = thingSpeakRead(channelID, ...
+    'Fields', [1 2 3 4 5 6], ...
+    'NumPoints', 100, ...
+    'ReadKey', readAPIKey);
+
+% Asignación de variables según los campos del canal
+voltaje = data(:,1);
+corriente = data(:,2);
+frecuencia = data(:,3);
+energia = data(:,4);
+potencia = data(:,5);
+factorPotencia = data(:,6);
+
+% Creación de gráficas de tiempo
+figure;
+
+subplot(3,2,1)
+plot(timeStamps, voltaje, '-o')
+title('Voltaje en el tiempo')
+xlabel('Tiempo')
+ylabel('Voltaje (V)')
+grid on
+
+subplot(3,2,2)
+plot(timeStamps, corriente, '-o')
+title('Corriente en el tiempo')
+xlabel('Tiempo')
+ylabel('Corriente (A)')
+grid on
+
+subplot(3,2,3)
+plot(timeStamps, frecuencia, '-o')
+title('Frecuencia en el tiempo')
+xlabel('Tiempo')
+ylabel('Frecuencia (Hz)')
+grid on
+
+subplot(3,2,4)
+plot(timeStamps, energia, '-o')
+title('Energía en el tiempo')
+xlabel('Tiempo')
+ylabel('Energía (kWh)')
+grid on
+
+subplot(3,2,5)
+plot(timeStamps, potencia, '-o')
+title('Potencia en el tiempo')
+xlabel('Tiempo')
+ylabel('Potencia (W)')
+grid on
+
+subplot(3,2,6)
+plot(timeStamps, factorPotencia, '-o')
+title('Factor de Potencia en el tiempo')
+xlabel('Tiempo')
+ylabel('Factor de Potencia')
+grid on
